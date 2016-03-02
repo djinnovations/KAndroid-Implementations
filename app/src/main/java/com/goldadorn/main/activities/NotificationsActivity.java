b24a49e3c2f7a90e2a3db734068704721e1950b1
bhkiran <kiran.b.h.mec@gmail.com>
2016-03-02 19:51:54 +0530

    notification

--- app/src/main/java/com/goldadorn/main/activities/NotificationsActivity.java
+++ app/src/main/java/com/goldadorn/main/activities/NotificationsActivity.java
@@ -2,6 +2,7 @@ package com.goldadorn.main.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -9,16 +10,21 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxStatus;
 import com.goldadorn.main.R;
 import com.goldadorn.main.utils.IDUtils;
-import com.kimeeo.library.ajax.ExtendedAjaxCallback;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.github.images.CircularImageView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -46,16 +52,71 @@ public class NotificationsActivity extends BaseActivity {
         refresh();
     }
 
-    private void refresh(){
-        Map<String, Object> params = new HashMap<>();
-        params.put("notification", "0");
+    private void refresh() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<Cookie> cookies = getApp().getCookies();
+                try {
+//                   OkHttpClient client = new OkHttpClient();
+//                   RequestBody formBody = new FormEncodingBuilder()
+//                           .add("notification", "0")
+//                           .build();
+//                   Request request = new Request.Builder()
+//                           .url("http://goldadorn.cloudapp.net/goldadorn_dev/rest/notifications")
+//                           .post(formBody)
+//                           .build();
+//
+//                   Response response = client.newCall(request).execute();
+//                   Log.d("Response",response.toString());
 
-        String url = getUrlHelper().getNotificationsUrl();
-        ExtendedAjaxCallback ajaxCallback =getAjaxCallback(postCallToken);
-        ajaxCallback.setClazz(String.class);
-        ajaxCallback.setParams(params);
-        ajaxCallback.method(AQuery.METHOD_POST);
-        getAQuery().ajax(url, params, String.class, ajaxCallback);
+
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpPost httppost = new HttpPost("http://goldadorn.cloudapp.net/goldadorn_dev/rest/notifications/");
+
+                    try {
+
+                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                        String phpSession = "";
+
+                        if (cookies.isEmpty()) {
+                            Log.i("TAG", "None");
+                        } else {
+                            for (int i = 0; i < cookies.size(); i++) {
+                                phpSession += "auths"+"="+cookies.get(i).getValue()+";";
+
+                            }
+                            Log.i("session", phpSession);
+                        }
+                        httppost.addHeader("Cookie", phpSession);
+//                        nameValuePairs.add(new BasicNameValuePair("Cookie", phpSession));
+//                       nameValuePairs.add(new BasicNameValuePair("id", "123"));
+//                       nameValuePairs.add(new BasicNameValuePair("string", "Hey"));
+                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                        // Execute HTTP Post Request
+                        HttpResponse response = httpclient.execute(httppost);
+                        Log.d("Response", response.toString());
+                    } catch (Exception e) {
+                        // Catch Protocol Exception
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("notification", "0");
+//        String url = getUrlHelper().getNotificationsUrl();
+//        ExtendedAjaxCallback ajaxCallback =getAjaxCallback(postCallToken);
+//        ajaxCallback.setClazz(String.class);
+//        ajaxCallback.setParams(params);
+//        ajaxCallback.method(AQuery.METHOD_POST);
+//        getAQuery().ajax(url, params, String.class, ajaxCallback);
     }
 
     @Override
