9d2d6b879dd4959f0532b91eee811b9d3b0df366
nithinjohn <nithinjohnthaliyath@gmail.com>
2016-03-24 11:29:44 +0530

    test paymnet in progres

--- app/src/main/java/com/goldadorn/main/activities/showcase/TestActivity.java
+++ app/src/main/java/com/goldadorn/main/activities/showcase/TestActivity.java
@@ -1,108 +1,361 @@
 package com.goldadorn.main.activities.showcase;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
+import android.content.Context;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.goldadorn.main.R;
-import com.payu.custombrowser.Bank;
-import com.payu.custombrowser.PayUWebChromeClient;
-import com.payu.custombrowser.PayUWebViewClient;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by Vijith Menon on 6/3/16.
  */
 public class TestActivity extends Activity {
-//    private WebView mWebView;
-//    private BroadcastReceiver mReceiver = null;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.test_activity);
+
+    String TAG = "TestActivity";
+
+    /**
+     * Adding WebView as setContentView
+     */
+    WebView webView;
+
+    /**
+     * Context for Activity
+     */
+    Context activity;
+    /**
+     * Order Id
+     * To Request for Updating Payment Status if Payment Successfully Done
+     */
+    int mId; //Getting from Previous Activity
+    /**
+     * Required Fields
+     */
+    // Test Variables
+    /*
+    private String mMerchantKey = "FCyqqZ";
+    private String mSalt = "sfBpGA8E";
+    private String mBaseURL = "https://test.payu.in";
+    */
+
+    // Final Variables
+    private String mMerchantKey = "Your Merchant Key";
+    private String mSalt = "Salt";
+    private String mBaseURL = "https://secure.payu.in";
+
+
+    private String mAction = ""; // For Final URL
+    private String mTXNId; // This will create below randomly
+    private String mHash; // This will create below randomly
+    private String mProductInfo = "Food Items"; //Passing String only
+    private String mFirstName; // From Previous Activity
+    private String mEmailId; // From Previous Activity
+    private double mAmount; // From Previous Activity
+    private String mPhone; // From Previous Activity
+    private String mServiceProvider = "payu_paisa";
+    private String mSuccessUrl = "test://succ";
+    private String mFailedUrl = "test://fail";
+
+
+    boolean isFromOrder;
+    /**
+     * Handler
+     */
+    Handler mHandler = new Handler();
+
+    /**
+     * @param savedInstanceState
+     */
+    @SuppressLint({"AddJavascriptInterface", "SetJavaScriptEnabled"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        super.onCreate(savedInstanceState);
+
+        /**
+         * Setting WebView to Screen
+         */
+        setContentView(R.layout.test_activity);
+
+        /**
+         * Creating WebView
+         */
+        webView = (WebView) findViewById(R.id.web_view);
+
+        /**
+         * Context Variable
+         */
+        activity = getApplicationContext();
+
+
+        /**
+         * Getting Intent Variables...
+         */
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+            mFirstName = bundle.getString("name");
+            mEmailId = bundle.getString("email");
+            mAmount = bundle.getDouble("amount");
+            mPhone = bundle.getString("phone");
+            mId = bundle.getInt("id");
+            isFromOrder = bundle.getBoolean("isFromOrder");
+
+            Log.i(TAG, "" + mFirstName + " : " + mEmailId + " : " + mAmount + " : " + mPhone);
+
+            /**
+             * Creating Transaction Id
+             */
+            Random rand = new Random();
+            String randomString = Integer.toString(rand.nextInt()) + (System.currentTimeMillis() / 1000L);
+            mTXNId = hashCal("SHA-256", randomString).substring(0, 20);
+
+            mAmount = new BigDecimal(mAmount).setScale(0, RoundingMode.UP).intValue();
+
+            /**
+             * Creating Hash Key
+             */
+            mHash = hashCal("SHA-512", mMerchantKey + "|" +
+                    mTXNId + "|" +
+                    mAmount + "|" +
+                    mProductInfo + "|" +
+                    mFirstName + "|" +
+                    mEmailId + "|||||||||||" +
+                    mSalt);
+
+            /**
+             * Final Action URL...
+             */
+            mAction = mBaseURL.concat("/_payment");
+
+            /**
+             * WebView Client
+             */
+            webView.setWebViewClient(new WebViewClient() {
+
+                @Override
+                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                    super.onReceivedError(view, request, error);
+                    Toast.makeText(activity, "Oh no! " + error, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onReceivedSslError(WebView view,
+                                               SslErrorHandler handler, SslError error) {
+                    Toast.makeText(activity, "SSL Error! " + error, Toast.LENGTH_SHORT).show();
+                    handler.proceed();
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    return super.shouldOverrideUrlLoading(view, url);
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+
+                    if (url.equals(mSuccessUrl)) {
+//                        Intent intent = new Intent(PayUMoneyActivity.this, PaymentStatusActivity.class);
+//                        intent.putExtra("status", true);
+//                        intent.putExtra("transaction_id", mTXNId);
+//                        intent.putExtra("id", mId);
+//                        intent.putExtra("isFromOrder", isFromOrder);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                        startActivity(intent);
+                    } else if (url.equals(mFailedUrl)) {
+//                        Intent intent = new Intent(PayUMoneyActivity.this, PaymentStatusActivity.class);
+//                        intent.putExtra("status", false);
+//                        intent.putExtra("transaction_id", mTXNId);
+//                        intent.putExtra("id", mId);
+//                        intent.putExtra("isFromOrder", isFromOrder);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                        startActivity(intent);
+                    }
+                    super.onPageFinished(view, url);
+                }
+            });
+
+            webView.setVisibility(View.VISIBLE);
+            webView.getSettings().setBuiltInZoomControls(true);
+            webView.getSettings().setCacheMode(2);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.clearHistory();
+            webView.clearCache(true);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setSupportZoom(true);
+            webView.getSettings().setUseWideViewPort(false);
+            webView.getSettings().setLoadWithOverviewMode(false);
+            webView.addJavascriptInterface(new PayUJavaScriptInterface(TestActivity.this), "PayUMoney");
+
+            /**
+             * Mapping Compulsory Key Value Pairs
+             */
+            Map<String, String> mapParams = new HashMap<>();
+
+            mapParams.put("key", mMerchantKey);
+            mapParams.put("txnid", mTXNId);
+            mapParams.put("amount", String.valueOf(mAmount));
+            mapParams.put("productinfo", mProductInfo);
+            mapParams.put("firstname", mFirstName);
+            mapParams.put("email", mEmailId);
+            mapParams.put("phone", mPhone);
+            mapParams.put("surl", mSuccessUrl);
+            mapParams.put("furl", mFailedUrl);
+            mapParams.put("hash", mHash);
+            mapParams.put("service_provider", mServiceProvider);
+
+            webViewClientPost(webView, mAction, mapParams.entrySet());
+        } else {
+            Toast.makeText(activity, "Something went wrong, Try again.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Posting Data on PayUMoney Site with Form
+     *
+     * @param webView
+     * @param url
+     * @param postData
+     */
+    public void webViewClientPost(WebView webView, String url,
+                                  Collection<Map.Entry<String, String>> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<html><head></head>");
+        sb.append("<body onload='form1.submit()'>");
+        sb.append(String.format("<form id='form1' action='%s' method='%s'>", url, "post"));
+
+        for (Map.Entry<String, String> item : postData) {
+            sb.append(String.format("<input name='%s' type='hidden' value='%s' />", item.getKey(), item.getValue()));
+        }
+        sb.append("</form></body></html>");
+
+        Log.d("TAG", "webViewClientPost called: " + sb.toString());
+        webView.loadData(sb.toString(), "text/html", "utf-8");
+    }
+
+    /**
+     * Hash Key Calculation
+     *
+     * @param type
+     * @param str
+     * @return
+     */
+    public String hashCal(String type, String str) {
+        byte[] hashSequence = str.getBytes();
+        StringBuffer hexString = new StringBuffer();
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance(type);
+            algorithm.reset();
+            algorithm.update(hashSequence);
+            byte messageDigest[] = algorithm.digest();
+
+            for (int i = 0; i < messageDigest.length; i++) {
+                String hex = Integer.toHexString(0xFF & messageDigest[i]);
+                if (hex.length() == 1)
+                    hexString.append("0");
+                hexString.append(hex);
+            }
+        } catch (NoSuchAlgorithmException NSAE) {
+        }
+        return hexString.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onPressingBack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        onPressingBack();
+    }
+
+    /**
+     * On Pressing Back
+     * Giving Alert...
+     */
+    private void onPressingBack() {
+
+//        final Intent intent;
 //
-//        webView = (WebView) findViewById(R.id.webview);
-//        try {
-//            Class.forName("com.payu.custombrowser.Bank");
-//            final Bank bank = new Bank() {
-//                @Override
-//                public void registerBroadcast(BroadcastReceiver broadcastReceiver, IntentFilter filter) {
-//                    mReceiver = broadcastReceiver;
-//                    registerReceiver(broadcastReceiver, filter);
-//                }
+//        if (isFromOrder)
+//            intent = new Intent(PayUMoneyActivity.this, ProductInCartList.class);
+//        else
+//            intent = new Intent(PayUMoneyActivity.this, MainActivity.class);
 //
-//                @Override
-//                public void unregisterBroadcast(BroadcastReceiver broadcastReceiver) {
-//                    if(mReceiver != null){
-//                        unregisterReceiver(mReceiver);
-//                        mReceiver = null;
-//                    }
-//                }
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 //
-//                @Override
-//                public void onHelpUnavailable() {
-//                    findViewById(R.id.parent).setVisibility(View.GONE);
-//                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
-//                }
+//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(PayUMoneyActivity.this);
 //
-//                @Override
-//                public void onBankError() {
-//                    findViewById(R.id.parent).setVisibility(View.GONE);
-//                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
-//                }
+//        // Setting Dialog Title
+//        alertDialog.setTitle("Warning");
 //
-//                @Override
-//                public void onHelpAvailable() {
-//                    findViewById(R.id.parent).setVisibility(View.VISIBLE);
-//                }
-//            };
-//            Bundle args = new Bundle();
-//            args.putInt("webView", R.id.webview);
-//            args.putInt("tranLayout",R.id.trans_overlay);
-//            args.putInt("mainLayout",R.id.r_layout);
+//        // Setting Dialog Message
+//        alertDialog.setMessage("Do you cancel this transaction?");
 //
-//            String [] list =  payuConfig.getData().split("&");
-//            String txnId = null;
-//            String merchantKey = null;
-//            for (String item : list) {
-//                if(item.contains("txnid")){
-//                    txnId = item.split("=")[1];
-//                }else if (item.contains("key")){
-//                    merchantKey = item.split("=")[1];
-//                }
-//                if (null != txnId && null != merchantKey) break;
-//            }
-//            args.putString(Bank.TXN_ID, txnId == null ? String.valueOf(System.currentTimeMillis()) : txnId);
-//            args.putString("merchantid", null != merchantKey ? merchantKey : "could not find");
-//            PayUSdkDetails payUSdkDetails = new PayUSdkDetails();
-//            args.putString("sdkcode", "VersionCode: " + payUSdkDetails.getSdkVersionCode() + " VersionName: " + payUSdkDetails.getSdkVersionName());
-//            if(getIntent().getExtras().containsKey("showCustom")) {
-//                args.putBoolean("showCustom", getIntent().getBooleanExtra("showCustom", false));
-//            }
-//            args.putBoolean("showCustom", true);
-//            bank.setArguments(args);
-//            findViewById(R.id.parent).bringToFront();
-//            try {
-//                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.cb_fade_in, R.anim.cb_face_out).add(R.id.parent, bank).commit();
-//            }catch(Exception e)
-//            {
-//                e.printStackTrace();
+//        // On pressing Settings button
+//        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
 //                finish();
+//                startActivity(intent);
 //            }
-//            mWebView.setWebChromeClient(new PayUWebChromeClient(bank));
-//            mWebView.setWebViewClient(new PayUWebViewClient(bank));
-//        }catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+//        });
 //
-//        mWebView.getSettings().setJavaScriptEnabled(true);
-//        mWebView.getSettings().setDomStorageEnabled(true);
-//        mWebView.postUrl(PAYMENT_URL, POSTDATA, "base64"));
-//        // In case of using PayU PG
-//        //url = payuConfig.getEnvironment() == PayuConstants.PRODUCTION_ENV?  PayuConstants.PRODUCTION_PAYMENT_URL : //PayuConstants.MOBILE_TEST_PAYMENT_URL ;
-//        //webView.postUrl(url, EncodingUtils.getBytes(payuConfig.getData(), "base64"));
-//    }
+//        // on pressing cancel button
+//        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        // Showing Alert Message
+//        alertDialog.show();
+    }
+
+    public class PayUJavaScriptInterface {
+        Context mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        PayUJavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        public void success(long id, final String paymentId) {
+            mHandler.post(new Runnable() {
+
+                public void run() {
+                    mHandler = null;
+                    Toast.makeText(TestActivity.this, "Payment Successfully.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 }
