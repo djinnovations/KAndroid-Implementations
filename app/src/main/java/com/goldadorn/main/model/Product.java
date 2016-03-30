bbdaa781d134bbb498914606177cd42d847bd053
nithinjohn <nithinjohnthaliyath@gmail.com>
2016-03-30 20:12:12 +0530

    productdetail extends product done

--- app/src/main/java/com/goldadorn/main/model/Product.java
+++ app/src/main/java/com/goldadorn/main/model/Product.java
@@ -4,7 +4,7 @@ package com.goldadorn.main.model;
  * Created by Kiran BH on 06/03/16.
  */
 public class Product {
-    public final int id;
+    public int id;
     public String name, description, imageUrl;
     public float unitPrice;
     public static final String currency = " RS";
