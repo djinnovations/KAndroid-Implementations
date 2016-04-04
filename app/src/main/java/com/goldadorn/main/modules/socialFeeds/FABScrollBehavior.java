c8145cceb9231ccae5adbef566d9d383a51b5f24
BhavinPadhiyar <bhavin.in@gmail.com>
2016-04-04 16:02:37 +0530

    Style updated

--- app/src/main/java/com/goldadorn/main/modules/socialFeeds/FABScrollBehavior.java
+++ app/src/main/java/com/goldadorn/main/modules/socialFeeds/FABScrollBehavior.java
@@ -24,14 +24,36 @@ public class FABScrollBehavior extends CoordinatorLayout.Behavior {
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
-    Handler handler = new Handler();
+    Handler handler;
     Runnable runnablelocal;
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout,final View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
 
+        if(dyConsumed > 0)
+        {
+            if (runnablelocal != null) {
+                handler.removeCallbacks(runnablelocal);
+                handler = null;
+                runnablelocal=null;
+            }
+
+            runnablelocal = new Runnable() {
+                @Override
+                public void run() {
+                    child.setVisibility(View.VISIBLE);
+                    YoYo.with(Techniques.FadeInUp).duration(300).playOn(child);
+                }
+            };
+
+            handler = new Handler();
+            handler.postDelayed(runnablelocal, 3000);
+        }
+
         if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE)
         {
+
+
             YoYo.with(Techniques.FadeOutDown).duration(300).withListener(new Animator.AnimatorListener() {
 
                 @Override
@@ -42,27 +64,10 @@ public class FABScrollBehavior extends CoordinatorLayout.Behavior {
                 @Override
                 public void onAnimationEnd(Animator animator) {
                     child.setVisibility(View.GONE);
-
-
-                    if (runnablelocal != null) {
-                        handler.removeCallbacks(runnablelocal);
-                        runnablelocal=null;
-                    }
-
-                    runnablelocal = new Runnable() {
-                        @Override
-                        public void run() {
-                            child.setVisibility(View.VISIBLE);
-                            YoYo.with(Techniques.FadeInUp).duration(300).playOn(child);
-                        }
-                    };
-                    handler.postDelayed(runnablelocal, 3000);
-                    
                 }
 
                 @Override
                 public void onAnimationCancel(Animator animator) {
-
                 }
 
                 @Override
@@ -70,6 +75,8 @@ public class FABScrollBehavior extends CoordinatorLayout.Behavior {
 
                 }
             }).playOn(child);
+
+
 
         }
         else if(dyConsumed < 0 && child.getVisibility() == View.GONE)
